33cc2eebcb1f91c2f7ff2383acf04d5d9604e4c5
Jdmcc890 <55810535+Jdmcc890@users.noreply.github.com>
2020-11-03 14:28:38 -0600

    Asset Updates

---
+++ New Unity Project/Assets/Scripts/OurScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OurScript : MonoBehaviour
+{
+  
+    public AudioClip Anthem;
+    public AudioSource audioSource;
+ 
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Enters");
+            GetComponent<AudioSource>().clip = Anthem;
+            audioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Exits");
+            audioSource.Pause();
+        }
+    }
+}
