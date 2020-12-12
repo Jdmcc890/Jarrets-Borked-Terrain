7b610255ee4e4c5f5052a2b44f3ef1c904cfcf1f
Jdmcc890 <55810535+Jdmcc890@users.noreply.github.com>
2020-12-11 19:42:32 -0600

    Changes

---
+++ New Unity Project/Assets/Scripts/ForestLighting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForestLighting : MonoBehaviour
+{
+    public BoxCollider ForestTrigger;
+    public BoxCollider ForestTrigger1;
+    public Material ForestSkybox;
+    public Material currentSkybox;
+    public Material NormalSkybox;
+
+    public void Start()
+    {
+        currentSkybox = RenderSettings.skybox;
+        Debug.Log(currentSkybox);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + " entered the collider");
+        if(other.name == "Player" && currentSkybox == NormalSkybox)
+        {
+            Debug.Log("Loading Night Scene");
+            RenderSettings.skybox = ForestSkybox;
+            RenderSettings.fog = true;
+            GameObject.Find("DayLighting").SetActive(false);
+            GameObject.Find("NightLighting").SetActive(true);
+        }
+
+        if (other.tag == "Player" && currentSkybox == ForestSkybox)
+        {
+            Debug.Log("Loading Day Scene");
+            RenderSettings.skybox = NormalSkybox;
+            RenderSettings.fog = false;
+            GameObject.Find("DayLighting").SetActive(true);
+            GameObject.Find("NightLighting").SetActive(false);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/HiddenDoor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenDoor : MonoBehaviour
+{
+    public GameObject door;
+    public Collider range;
+    public Vector3 pivot1;
+    public Vector3 pivot2;
+    public float speed;
+    public bool inRange;
+
+
+    public void Update()
+    {
+        if(inRange == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Opening Door");
+            OpenDoor();
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player is in Range");
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player is NOT in Range");
+            inRange = false;
+        }
+    }
+
+
+    public void OpenDoor()
+    {
+        transform.position = Vector3.MoveTowards(door.transform.position, pivot1, 4);
+        transform.position = Vector3.MoveTowards(pivot1, pivot2, 10);
+    }
+}
